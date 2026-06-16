1ee20963b353e28bd8cb4a5b915bf69d61588ef0
swethakadhir <kadhirswetha8@gmail.com>
2026-06-16 11:24:06 +0530

    Use langchain-text-splitters package

--- rag_engine.py
+++ rag_engine.py
@@ -1,7 +1,7 @@
 import os
 
 from pypdf import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.chains import RetrievalQA
